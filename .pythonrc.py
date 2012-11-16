a6ddfe0a7ae762c39f8bcdb699007fa3b7674f30
Trinh Hai-Anh <anh.hai.trinh@gmail.com>
2012-11-16 11:51:19 +0700

    .pythonrc.py: readline and rlcompleter

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,4 +1,20 @@
+import atexit
+import readline
+import rlcompleter
+import os
 import sys
+
+from pprint import pprint as pp
+from pprint import pformat as pf
+
+try:
+    history = os.path.expanduser('~/.python_history')
+    open(history, 'a').close()
+    readline.read_history_file(history)
+    readline.parse_and_bind('tab: complete')
+    atexit.register(readline.write_history_file, history)
+except ImportError:
+    pass
 
 sys.ps1 = "pass; "
 sys.ps2 = ""
